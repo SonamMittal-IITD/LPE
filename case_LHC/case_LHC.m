e5cb222f1c32c60f6c4d496fa3c76396dd90c91b
Sonam Mittal <104248775+SonamMittal-IITD@users.noreply.github.com>
2022-04-25 12:23:04 +0530

    Add files via upload

--- case_LHC/case_LHC.m
+++ case_LHC/case_LHC.m
@@ -1,1 +1,61 @@
+function mpc = case_LHC
+%function [mpc, success, iterations] = radial_pf(mpc,mpopt)
 
+
+%% MATPOWER Case Format : Version 2
+mpc.version = '2';
+
+%%-----  Power Flow Data  -----%%
+%% system MVA base
+mpc.baseMVA = 0.1;
+
+%% bus data
+%	bus_i	type	Pd	Qd	Gs	Bs	area	Vm	Va	baseKV	zone	Vmax	Vmin
+mpc.bus = [  %% (Pd and Qd are specified in kW & kVAr here, converted to MW & MVAr below)
+	1	3	0	0	0	0	1	1	0	0.400	1	1	1;
+	2	1	100	75	0	0	1	1	0	0.400	1	1.1	0.9;
+	3	1	8.5	4	0	0	1	1	0	0.400	1	1.1	0.9;
+	4	1	50	37	0	0	1	1	0	0.400	1	1.1	0.9;
+	5	1	38.9	17	0	0	1	1	0	0.400	1	1.1	0.9;
+	6	1	74.7	36.5	0	0	1	1	0	0.400	1	1.1	0.9;
+	
+];
+
+%% generator data
+%	bus	Pg	Qg	Qmax	Qmin	Vg	mBase	status	Pmax	Pmin	Pc1	Pc2	Qc1min	Qc1max	Qc2min	Qc2max	ramp_agc	ramp_10	ramp_30	ramp_q	apf
+mpc.gen = [
+	1	0	0	10	-10	1	0.1	1	10	0	0	0	0	0	0	0	0	0	0	0	0;
+];
+
+%% branch data
+%	fbus	tbus	r	x	b	rateA	rateB	rateC	ratio	angle	status	angmin	angmax
+mpc.branch = [  %% (r and x specified in ohms here, converted to p.u. below)
+	1	2	0.066	0.0365	0	0	0	0	0	0	1	-360	360;
+	1	3	0.138	0.073	0	0	0	0	0	0	1	-360	360;
+	1	4	0.066	0.0365	0	0	0	0	0	0	1	-360	360;
+	1	5	0.207	0.1095	0	0	0	0	0	0	1	-360	360;
+	4	6	0.138	0.073	0	0	0	0	0	0	1	-360	360;
+	
+];
+
+%%-----  OPF Data  -----%%
+%% generator cost data
+%	1	startup	shutdown	n	x1	y1	...	xn	yn
+%	2	startup	shutdown	n	c(n-1)	...	c0
+mpc.gencost = [
+	2	0	0	3	0	20	0;
+];
+
+
+%% convert branch impedances from Ohms to p.u.
+[PQ, PV, REF, NONE, BUS_I, BUS_TYPE, PD, QD, GS, BS, BUS_AREA, VM, ...
+    VA, BASE_KV, ZONE, VMAX, VMIN, LAM_P, LAM_Q, MU_VMAX, MU_VMIN] = idx_bus;
+[F_BUS, T_BUS, BR_R, BR_X, BR_B, RATE_A, RATE_B, RATE_C, ...
+    TAP, SHIFT, BR_STATUS, PF, QF, PT, QT, MU_SF, MU_ST, ...
+    ANGMIN, ANGMAX, MU_ANGMIN, MU_ANGMAX] = idx_brch;
+Vbase = mpc.bus(1, BASE_KV) * 1e3;      %% in Volts
+Sbase = mpc.baseMVA * 1e6;              %% in VA
+mpc.branch(:, [BR_R BR_X]) = mpc.branch(:, [BR_R BR_X]) / (Vbase^2 / Sbase);
+
+%% convert loads from kW to MW
+mpc.bus(:, [PD, QD]) = mpc.bus(:, [PD, QD]) / 1e3;
